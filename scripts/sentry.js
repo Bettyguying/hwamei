d93a65d0bf68ad82b3edc7735b17755edd88998f
likai <youyuge@gmail.com>
2018-04-28 14:52:41 +0800

    增加 sentry 报告

--- scripts/sentry.js
+++ scripts/sentry.js
@@ -12,8 +12,8 @@
 //   sentry report
 
 require('dotenv').config()
-const tz = 'Asia/Shanghai'
 const cronJob = require('cron').CronJob
+const wxwork = require('../lib/wxwork')
 const request = require('request')
 
 const SENTRY_BASE_URL = process.env.SENTRY_BASE_URL
@@ -21,55 +21,72 @@ const SENTRY_AUTH_HEADER = {
   'Authorization': `Bearer ${process.env.SENTRY_TOKEN}`
 }
 
-module.exports = (robot) => {
+const projectToChats = {
+  'uniqueway/cure': 'cure',
+  'uniqueway/unicrm': 'unicrm',
+  'uniqueway/unicrm_backend': 'unicrm',
+  'uniqueway/dist_backend': 'dist',
+  'uniqueway/dist_frontend': 'dist',
+  'uniqueway/similan_backend': 'similan',
+  'uniqueway/traveler-api': 'traveller',
+  'uniqueway/traveler-frontend': 'traveller',
+  'uniqueway/uniapp': 'uniapp',
+  'uniqueway/www': 'www',
+  'uniqueway/deal_distrubition': 'DealDistribution',
+  'uniqueway/shallowPlan': 'research'
+}
 
-  // new cronJob('* * * * * *', reportSentryExceptionStats, null, true, tz) // 注册定时任务
+module.exports = (robot) => {
+  const tz = 'Asia/Shanghai'
+  new cronJob('0 11 * * * *', reportAllProjectStats, null, true, tz)
 
   robot.hear(/sentry report for (.*)/, (res) => {
-    const chatId = res.match[1]
-    const message = buildReportMessage(chatId)
-    sendMessage(chatId, message)
+    const project = res.match[1]
+    reportProjectStats(project)
+  })
+
+  robot.hear(/sentry report all/, (res) => {
+    reportAllProjectStats()
   })
 }
 
-async function sendMessage(chatId, message){
-  const response = await wxwork.sendMessage(chatId, message)
-  if (response.ok) {
-    return 'Message was send'
-  } else {
-    return response.message
+function reportProjectStats(project) {
+  const chatId = projectToChats[project]
+  reportProjectStatToChat(chatId, project)
+}
+
+function reportAllProjectStats() {
+  for(project in projectToChats) {
+    const chatId = projectToChats[project]
+    reportProjectStatToChat(chatId, project)
   }
 }
 
-function sentryReport() {
+function reportProjectStatToChat(chatId, project) {
   request({
-    url: `${SENTRY_BASE_URL}/projects/`,
+    url: `${SENTRY_BASE_URL}/api/0/projects/${project}/issues/?statsPeriod=24h`,
     headers: SENTRY_AUTH_HEADER
   }, (error, response, body) => {
-    let projects = JSON.parse(body)
-    let lines = []
-    for (let project of projects) {
-      const since = new Date().setHours(0, 0, 0, 0) / 1000
-      const count = fetchProjectExceptionCount(project, since)
-      lines.push(`项目: ${project['name']}，今天到目前为止共有 ${count} 个错误`)
+    try {
+      let periods = JSON.parse(body)
+      let count = periods.length
+      let message = `【sentry】${project} `
+
+      if (count == undefined) {
+        message += `应该是不存在`
+      } else if (count == 0) {
+        message += `到目前为止没有任何错误，非常非常棒！继续保持啊！`
+      } else {
+        message += `到目前为止还有 ${count} 个错误没有得到解决。`
+      }
+
+      message += `${SENTRY_BASE_URL}/${project}`
+
+      console.log(message)
+      wxwork.sendMessage(chatId, message)
+    } catch(error) {
+      console.log(project)
+      console.error(error)
     }
-
-    return lines.join("\n")
-  })
-}
-
-function fetchProjectExceptionCount(project, since) {
-  request({
-    url: `${SENTRY_BASE_URL}/${project['slug']}/stats/?since=${since}`,
-    headers: SENTRY_AUTH_HEADER
-  }, (error, response, body) => {
-    let count = 0
-    let periods = JSON.parse(body)
-
-    for (let period of periods) {
-      count += period[1]
-    }
-
-    return count
   })
 }
